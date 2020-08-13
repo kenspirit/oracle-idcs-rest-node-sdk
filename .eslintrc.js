a4ff2de0f2ee543aff5d2cd8e24fb569bccfbcb8
Ken Chen <ken.chen@oocl.com>
2020-08-13 17:29:20 +0800

    Support setting bypassNotification when resetting password

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  extends: 'airbnb-base'
+  extends: 'airbnb-base',
+  rules: {
+    'space-before-function-paren': 0,
+    // 'no-console': ['warn'],
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': ['error', 'never'],
+  }
 }
